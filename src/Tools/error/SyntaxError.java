21f60bef3ad50867dd616e3f9ac2f70e38ed02c0
Parsifal1986 <parsifal1986@sjtu.edu.cn>
2024-08-12 15:47:15 +0800

    Delete error perfix "SyntaxError"

--- src/Tools/error/SyntaxError.java
+++ src/Tools/error/SyntaxError.java
@@ -4,6 +4,7 @@ import Tools.Position;
 
 public class SyntaxError extends Error {
     public SyntaxError(String msg, Position pos) {
-        super("SyntaxError: " + msg, pos);
+        // super("SyntaxError: " + msg, pos);
+        super(msg, pos);
     }
 }
