15f93216b73fec7b52069f4d99d3591c32f20f55
Parsifal1986 <parsifal1986@sjtu.edu.cn>
2024-08-12 17:03:11 +0800

    Let all the errors that is thrown by antlr be "Invalid Identifier"

--- src/Tools/MxErrorListener.java
+++ src/Tools/MxErrorListener.java
@@ -9,6 +9,7 @@ import Tools.error.SyntaxError;
 public class MxErrorListener extends BaseErrorListener {
   @Override
   public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-    throw new SyntaxError(msg, new Position(line, charPositionInLine));
+    // throw new SyntaxError(msg, new Position(line, charPositionInLine));
+    throw new SyntaxError("Invalid Identifier", new Position(line, charPositionInLine));
   }
 }
