c78117906acea4c366a519db3b97fd5e38992c00
Parsifal1986 <parsifal1986@sjtu.edu.cn>
2024-10-30 11:10:31 +0800

    prevent repeatly storage

--- src/Tools/IRsema/call.java
+++ src/Tools/IRsema/call.java
@@ -3,6 +3,7 @@ package Tools.IRsema;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 import Tools.Entity;
 import Tools.RISCVsema.arithmetic_i;
@@ -54,6 +55,7 @@ public class call extends statement {
     register ra = regAlloc.raSaveRegister;
     ret.addAll(regAlloc.StorePhyReg(regAlloc.GetPhyReg("ra"), regAlloc.GetVirtReg(ra)));
     HashMap<Integer, register> callerSavedMap = new HashMap<>();
+    HashSet<Integer> hashStored = new HashSet<>();
     int cnt;
     for (register a : liveVarOut) {
       if (callerSavedMap.containsKey(a.regId)) {
@@ -84,9 +86,10 @@ public class call extends statement {
         Entity whereToLoad = args.get(i);
         if (args.get(i) instanceof register) {
           virtreg reg = regAlloc.GetVirtReg((register)args.get(i));
-          if (reg.regId != -1 && 10 <= reg.regId && reg.regId < i + 10) {
+          if (reg.regId != -1 && 10 <= reg.regId && reg.regId < i + 10 && !hashStored.contains(reg.regId)) {
             ret.addAll(cnt, regAlloc.StorePhyReg(regAlloc.GetPhyReg(reg.regId), regAlloc.GetVirtReg(regAlloc.callerSaveRegMap.get(reg.regId))));
             whereToLoad = regAlloc.callerSaveRegList.get(reg.regId - 10).virtualReg;
+            hashStored.add(reg.regId);
           }
         }
         ret.addAll(regAlloc.LoadToPhyReg(regAlloc.GetPhyReg("a" + i), whereToLoad));
